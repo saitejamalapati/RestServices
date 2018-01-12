8e404c1d9975a2078e317c09171f554617e2b3c8
Malapati Venkata Sai Teja <34930251+saitejamalapati@users.noreply.github.com>
2018-01-12 09:09:55 +0530

    Update UserService.java

--- src/main/java/com/accolite/rest/RestProject/Service/UserService.java
+++ src/main/java/com/accolite/rest/RestProject/Service/UserService.java
@@ -1,6 +1,8 @@
 package com.accolite.rest.RestProject.Service;
 
+import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
@@ -17,12 +19,12 @@ public class UserService {
 		return "This is mock drill\n";
 	}
 	
-	public List<User> getUsers() {
+	public List<User> getUsers() throws ClassNotFoundException, SQLException {
 		List<User> userList = new ArrayList<User>();
 		try {
-			MyConnection conVar = MyConnection.getConnection();
+			Connection con = MyConnection.getConnection();
 			String query = "select * from user";
-			Statement stmt = conVar.getCon().createStatement();
+			Statement stmt = con.createStatement();
 			ResultSet rs = stmt.executeQuery(query);
 			while(rs.next()) {
 				int id, age;
@@ -34,19 +36,18 @@ public class UserService {
 				User temp = new User(id,username,age,city);
 				userList.add(temp);
 			}
-			conVar.getCon().close();
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
 		return userList;
 	}
 	
-	public User getUserByID(int ID) {
+	public User getUserByID(int ID) throws ClassNotFoundException, SQLException {
 		User temp = null;
 		try {
-			MyConnection conVar = MyConnection.getConnection();
+			Connection con = MyConnection.getConnection();
 			String query = "select * from user where ID="+ID;
-			Statement stmt = conVar.getCon().createStatement();
+			Statement stmt = con.createStatement();
 			ResultSet rs = stmt.executeQuery(query);
 			if(rs.next()) {
 				int id, age;
@@ -57,40 +58,40 @@ public class UserService {
 				city = rs.getString("city");
 				temp = new User(id,username,age,city);
 			}
-			conVar.getCon().close();
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
 	    return temp;
 	}
 	
-	public User addUser(User user) {
+	public User addUser(User user) throws ClassNotFoundException, SQLException {
 		try {
-			MyConnection conVar = MyConnection.getConnection();
+			Connection con = MyConnection.getConnection();
 			String query = "insert into user(username,age,city) values('" + user.getUserName() + "'," + user.getAge() + ",'" + user.getCity() + "')";
-			Statement stmt = conVar.getCon().createStatement();
+			Statement stmt = con.createStatement();
 			stmt.executeUpdate(query);
 			query = "select * from user where username='"+user.getUserName()+"'";
 			ResultSet rs = stmt.executeQuery(query);
-			user.setId(rs.getInt("ID"));
-			conVar.getCon().close();
+			if(rs.next()) {
+				user.setId(rs.getInt("ID"));
+			}
+
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
 	    return user;
 	}
 	
-	public User updateUser(int id, User user) {
+	public User updateUser(int id, User user) throws ClassNotFoundException, SQLException {
 		User temp = getUserByID(id);
 		if(temp!=null) {
 			temp.setCity(user.getCity());
 			temp.setAge(user.getAge());
 			try {
-				MyConnection conVar = MyConnection.getConnection();
+				Connection con = MyConnection.getConnection();
 				String query = "update user SET age="+temp.getAge()+",city='"+temp.getCity()+"' where ID="+temp.getId();
-				Statement stmt = conVar.getCon().createStatement();
+				Statement stmt = con.createStatement();
 				stmt.executeUpdate(query);
-				conVar.getCon().close();
 			}catch(Exception e) {
 				e.printStackTrace();
 			}
@@ -99,16 +100,15 @@ public class UserService {
 		return null;
 	}
 	
-	public User deleteUser(int id) {
+	public User deleteUser(int id) throws ClassNotFoundException, SQLException {
 		User temp = getUserByID(id);
 		if(temp == null)
 			return null;
 		try {
-			MyConnection conVar = MyConnection.getConnection();
+			Connection con = MyConnection.getConnection();
 			String query = "delete from user where ID="+id;
-			Statement stmt = conVar.getCon().createStatement();
+			Statement stmt = con.createStatement();
 			stmt.executeUpdate(query);
-			conVar.getCon().close();
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
